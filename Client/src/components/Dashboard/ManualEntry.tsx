636565861447000eef9be434837ede53b23b4eda
SahanHeshan <dshsamarathunga@gmail.com>
2024-03-24 17:18:15 +0530

    dashboard api connection made

--- Client/src/components/Dashboard/ManualEntry.tsx
+++ Client/src/components/Dashboard/ManualEntry.tsx
@@ -1,42 +1,86 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./ManualEntry.css";
 
+interface FormData {
+  numberPlate: string;
+  vehicleType: string;
+  status: "IN" | "OUT";
+}
+
 const ManualEntry: React.FC = () => {
-  const [numberPlate, setNumberPlate] = useState("");
-  const [vehicleType, setVehicleType] = useState("");
-  const [status, setStatus] = useState<"IN" | "Out">("IN");
+  const [formData, setFormData] = useState<FormData>({
+    numberPlate: "",
+    vehicleType: "OTHER",
+    status: "IN",
+  });
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
 
   const handleNumberPlateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setNumberPlate(e.target.value);
+    setFormData({ ...formData, numberPlate: e.target.value.toUpperCase() });
   };
 
-  const handleVehicleTypeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setVehicleType(e.target.value);
+  const handleVehicleTypeChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setFormData({ ...formData, vehicleType: e.target.value });
   };
 
-  const handleStatusChange = (newStatus: "IN" | "Out") => {
-    setStatus(newStatus);
+  const handleStatusChange = (newStatus: "IN" | "OUT") => {
+    setFormData({ ...formData, status: newStatus });
   };
 
-  // const handleClear = () => {
-  //   setNumberPlate("");
-  //   setVehicleType("");
-  //   setStatus("IN");
-  // };
-
-  const handleSubmit = (e: React.FormEvent) => {
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    // Submit logic goes here
-    console.log("Number Plate:", numberPlate);
-    console.log("Vehicle Type:", vehicleType);
-    console.log("Status:", status);
+
+    // Check if number plate is empty
+    if (formData.numberPlate.trim() === "") {
+      alert("Please enter a number plate.");
+      return;
+    }
+
+    try {
+      const entryDate = currentTime.toISOString().split("T")[0]; // YYYY-MM-DD
+      const entryTime = currentTime.toLocaleTimeString([], { hour12: false }); // HH:MM:SS
+      const response = await axios.post("http://127.0.0.1:5002/addEntry", {
+        entryDate,
+        entryTime,
+        ...formData,
+      });
+      console.log(response.data.message); // Log the response message
+
+      // Reset form fields
+      setFormData({
+        numberPlate: "",
+        vehicleType: "OTHER",
+        status: "IN",
+      });
+    } catch (error) {
+      console.error("Error:", error);
+    }
   };
 
   return (
     <div className="manual-entry">
       <div className="header">
         <h2>Manual Entry</h2>
-        <span className="timestamp">2024-03-07 12:26:30 PM</span>
+        <span className="timestamp">
+          {currentTime.toISOString().split("T")[0]}{" "}
+        </span>
+        <span className="timestamp">
+          {currentTime.toLocaleString("en-US", {
+            hour12: false,
+            hour: "numeric",
+            minute: "numeric",
+            second: "numeric",
+          })}
+        </span>
       </div>
       <form onSubmit={handleSubmit}>
         <div className="form-group">
@@ -44,39 +88,47 @@ const ManualEntry: React.FC = () => {
           <input
             type="text"
             id="numberPlate"
-            value={numberPlate}
+            value={formData.numberPlate}
             onChange={handleNumberPlateChange}
+            required
           />
         </div>
         <div className="form-group">
           <label htmlFor="vehicleType">Vehicle Type</label>
-          <input
-            type="text"
+          <select
             id="vehicleType"
-            value={vehicleType}
+            value={formData.vehicleType}
             onChange={handleVehicleTypeChange}
-          />
+          >
+            <option value="Other">Other</option>
+            <option value="Car">Car</option>
+            <option value="Motorcycle">Motorcycle</option>
+            <option value="Truck">Truck</option>
+            {/* Add more options as needed */}
+          </select>
         </div>
-
         <div className="form-group status">
           <label>Status</label>
           <div className="status-buttons">
             <button
               type="button"
-              className={`status-button ${status === "IN" ? "active" : ""}`}
+              className={`status-button ${
+                formData.status === "IN" ? "activegre" : ""
+              }`}
               onClick={() => handleStatusChange("IN")}
             >
               IN
             </button>
             <button
               type="button"
-              className={`status-button ${status === "Out" ? "activered" : ""}`}
-              onClick={() => handleStatusChange("Out")}
+              className={`status-button ${
+                formData.status === "OUT" ? "activered" : ""
+              }`}
+              onClick={() => handleStatusChange("OUT")}
             >
-              Out
+              OUT
             </button>
           </div>
-
           <div className="form-actions">
             <button type="submit" className="submit-button">
               Submit
--- Client/src/components/Dashboard/Previous.tsx
+++ Client/src/components/Dashboard/Previous.tsx
@@ -1,80 +1,31 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 import styles from "./Previous.module.css";
-
-interface Entry {
-  date: string;
-  time: string;
-  numberPlate: string;
-  vehicleType: string;
-  status: "in" | "out";
-}
-
-interface ApiResponse {
-  length: number;
-  result: {
-    day: {
-      year: number;
-      month: number;
-      date: number;
-      hour: number;
-      minute: number;
-      second: number;
-    };
-    number_plate: string;
-    vehicle_type: string;
-    status: string;
-  }[];
-}
+import { useEntries } from "./lastEntries";
 
 const Previous: React.FC = () => {
-  const [entries, setEntries] = useState<Entry[]>([]);
-
-  useEffect(() => {
-    const fetchEntries = async () => {
-      try {
-        const response = await axios.get<ApiResponse>(
-          "http://127.0.0.1:5002/lastEntry"
-        );
-        const { result } = response.data;
-        const entries: Entry[] = result.map((item) => ({
-          date: `${item.day.year}/${item.day.month}/${item.day.date}`,
-          time: `${item.day.hour}:${item.day.minute}:${item.day.second}`,
-          numberPlate: item.number_plate,
-          vehicleType:
-            item.vehicle_type.toLowerCase() === ""
-              ? "Other"
-              : item.vehicle_type,
-          status: item.status.toLowerCase() === "in" ? "in" : "out",
-        }));
-        setEntries(entries);
-      } catch (error) {
-        console.error("Error fetching entries:", error);
-      }
-    };
-
-    fetchEntries();
-  }, []);
+  const { previous } = useEntries();
 
   return (
     <div className={styles.container}>
       <div className={styles.header}>Previous Entries</div>
-      <div className={styles.entriesContainer}>
-        {entries.map((entry, index) => (
-          <div key={index} className={styles.entryContainer}>
-            <div className={styles.entryDetails}>
-              <div className={styles.date}>{entry.date}</div>
-              <div className={styles.time}>{entry.time}</div>
-              <div className={styles.numberPlate}>{entry.numberPlate}</div>
-              <div className={styles.vehicleType}>{entry.vehicleType}</div>
+      <div className={styles.scrollContainer}>
+        <div className={styles.entriesContainer}>
+          {previous.map((entry, index) => (
+            <div key={index} className={styles.entryContainer}>
+              <div className={styles.entryDetails}>
+                <div className={styles.date}>{entry.date}</div>
+                <div className={styles.time}>{entry.time}</div>
+                <div className={styles.numberPlate}>{entry.numberPlate}</div>
+                <div className={styles.vehicleType}>{entry.vehicleType}</div>
+              </div>
+              <div
+                className={`${styles.statusIndicator} ${
+                  entry.status === "in" ? styles.statusIn : styles.statusOut
+                }`}
+              />
             </div>
-            <div
-              className={`${styles.statusIndicator} ${
-                entry.status === "in" ? styles.statusIn : styles.statusOut
-              }`}
-            />
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
